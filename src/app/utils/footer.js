a593680301106d765b16cb10c3fb2dcedabbe104
Pablo Méndez <pablomendez778@gmail.com>
2021-03-01 00:29:31 -0600

    Detalles del footer que había olvidado cambiar.

--- src/app/utils/footer.js
+++ src/app/utils/footer.js
@@ -26,18 +26,26 @@ export function implementFooter(footer) {
 
     const bootstrap = document.createElement("div");
     bootstrap.classList.add("col-md-4", "col-sm-12");
-    addFooterContent(bootstrap, "Horarios", [
+    addFooterContent(bootstrap, "Bootstrap", [
         "Elegante",
         "Completo",
         "Fácil",
         "Conveniente",
     ]);
     container.appendChild(bootstrap);
+
+    const division = document.createElement("hr");
+    division.classList.add("light", "col-12");
+    container.appendChild(division);
+    const copyRight = document.createElement("p");
+    copyRight.classList.add("text-center", "col-12");
+    copyRight.innerText = "Méndez, apellido registrado cuando nací";
+    container.appendChild(copyRight);
 }
 
 function addFooterContent(container, titleText, texts){
     addHorizontalLine(container);
-    const title = document.createElement("h2");
+    const title = document.createElement("h3");
     title.innerText = titleText;
     container.appendChild(title);
     addHorizontalLine(container);
@@ -56,38 +64,3 @@ function addParagraphLine(container, text){
     paragraph.innerText = text;
     container.appendChild(paragraph);
 }
-
-/*
-
-    <div class="container-fluid padding">
-        <div class="row text-center padding">
-            <div class="col-md-4 col-sm-6">
-                <hr class="light">
-                <h1>Méndez</h1>
-                <hr class="light">
-                <p>Pablo Méndez</p>
-                <p>men19195@uvg.edu.gt</p>
-                <p>Guatemala, Guatemala</p>
-            </div>
-            <div class="col-md-4 col-sm-6">
-                <hr class="light">
-                <h1>Horarios</h1>
-                <hr class="light">
-                <p>Lunes a Jueves: 10am - 2am</p>
-                <p>Viernes: 7am - 12am</p>
-                <p>Sábados: 9am - 12am</p>
-                <p>Sábados: 10am - 3am</p>
-            </div>
-            <div class="col-md-4 col-sm-12">
-                <hr class="light">
-                <h1>Bootstrap</h1>
-                <hr class="light">
-                <p>Elegante</p>
-                <p>Fácil</p>
-                <p>Conveniente</p>
-                <p>Completo</p>
-            </div>
-        </div>
-    </div>
-
- */
