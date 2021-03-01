f9112b7d7cef56123491cfd5268c762997d3d01d
Pablo Méndez <pablomendez778@gmail.com>
2021-03-01 15:47:27 -0600

    Le puse música que se comparte en pestañas >:D

--- src/app/index/index.js
+++ src/app/index/index.js
@@ -1,31 +1,11 @@
 import "./main.scss"
-import gato from "../../assets/img/Gato con Lentes.jpg";
-import zum from "../../assets/img/Zum.png";
 import {implementNavigationBar} from "../utils/navigation";
 import {implementFooter} from "../utils/footer";
-import {implementCarousel} from "../utils/carousel";
+import {setMusicPlayer} from "../utils/music";
 
+setMusicPlayer();
 const navigationBar = document.getElementById("navigationBar");
 implementNavigationBar(navigationBar);
 
 const elegantFooter = document.getElementById("elegant-footer");
 implementFooter(elegantFooter);
-
-const carousel = document.getElementById("imageCarousel");
-implementCarousel(carousel,
-    [
-        {
-            src: gato,
-            active: true,
-            alt: "Imagen de un gato con lentes",
-            header: "Título",
-            text: "Esto es un texto"
-        },
-        {
-            src: zum,
-            active: false,
-            alt: "Imagen de Zum",
-            header: "Título Zum",
-            text: "Esto es un texto de Zum"
-        }
-    ]);
--- webpack.common.js
+++ webpack.common.js
@@ -26,6 +26,17 @@ module.exports = {
                     }
                 }
             },
+            {
+                test: /\.mp3$/i,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        esModule: false,
+                        name: "[name]-[hash].[ext]",
+                        outputPath: "audio",
+                    }
+                }
+            },
         ]
     },
 
