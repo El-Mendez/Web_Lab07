1d1861502de16b5f510b4d58d04acb5c1bba5202
Pablo Méndez <pablomendez778@gmail.com>
2021-03-02 21:57:51 -0600

    Si importara bien las cosas, entonces ya estarían los tags de spoiler

--- src/app/libros/libros.js
+++ src/app/libros/libros.js
@@ -2,6 +2,8 @@ import "./libros.scss"
 import {implementNavigationBar} from "../utils/navigation";
 import {implementFooter} from "../utils/footer";
 import {setMusicPlayer} from "../utils/music";
+// TODO hacer que los spoiler tags funcionen
+
 // TODO averiguar cómo se importan esas babosadas
 
 setMusicPlayer();
