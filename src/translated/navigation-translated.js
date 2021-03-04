ac78c1acea1fc0e4e297af657fea7bfa2c16af76
Pablo Méndez <pablomendez778@gmail.com>
2021-03-04 16:52:46 -0600

    Babel: Implementé el uso de presets

--- src/translated/navigation-translated.js
+++ src/translated/navigation-translated.js
@@ -1,15 +1,28 @@
-import { destinations } from "./destinations.js";
-import { brandName } from "./constants";
-import Popover from "bootstrap/js/src/popover";
-import "bootstrap/dist/js/bootstrap";
-export function implementNavigationBar(nav, current) {
-  const controllerId = "navBarContent";
-  const brandItem = document.createElement("a");
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.implementNavigationBar = implementNavigationBar;
+
+var _destinations = require("./destinations.js");
+
+var _constants = require("./constants");
+
+var _popover = _interopRequireDefault(require("bootstrap/js/src/popover"));
+
+require("bootstrap/dist/js/bootstrap");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function implementNavigationBar(nav, current) {
+  var controllerId = "navBarContent";
+  var brandItem = document.createElement("a");
   brandItem.classList.add("navbar-brand");
   brandItem.href = "index.html";
-  brandItem.innerText = brandName;
+  brandItem.innerText = _constants.brandName;
   nav.appendChild(brandItem);
-  const toggler = document.createElement("button");
+  var toggler = document.createElement("button");
   toggler.classList.add("navbar-toggler");
   toggler.type = "button";
   toggler.setAttribute("data-bs-toggle", "collapse");
@@ -17,19 +30,19 @@ export function implementNavigationBar(nav, current) {
   toggler.setAttribute("aria-controls", controllerId);
   toggler.setAttribute("aria-expanded", "false");
   toggler.setAttribute("aria-label", "Toggle Navigation");
-  const togglerIcon = document.createElement("span");
+  var togglerIcon = document.createElement("span");
   togglerIcon.classList.add("navbar-toggler-icon");
   toggler.appendChild(togglerIcon);
   nav.appendChild(toggler);
-  const elementsListContainer = document.createElement("div");
+  var elementsListContainer = document.createElement("div");
   elementsListContainer.classList.add("collapse", "navbar-collapse");
   elementsListContainer.id = controllerId;
   nav.appendChild(elementsListContainer);
-  const elementList = document.createElement("ul");
+  var elementList = document.createElement("ul");
   elementList.classList.add("navbar-nav");
   createDestinations(elementList, current);
   elementsListContainer.appendChild(elementList);
-  const muteButton = document.createElement("a");
+  var muteButton = document.createElement("a");
   muteButton.tabIndex = "0";
   muteButton.classList.add("btn", "btn-lg", "btn-danger");
   muteButton.role = "button";
@@ -39,18 +52,18 @@ export function implementNavigationBar(nav, current) {
   elementsListContainer.appendChild(muteButton);
   muteButton.innerText = "mute"; // Para que el botón de mute funcione
 
-  const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-  const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new Popover(popoverTriggerEl);
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new _popover["default"](popoverTriggerEl);
   });
 }
 
 function createDestinations(destinationList, current) {
-  for (let i = 0; i < destinations.length; i++) {
-    const destination = destinations[i];
-    let element = document.createElement("li");
+  for (var i = 0; i < _destinations.destinations.length; i++) {
+    var destination = _destinations.destinations[i];
+    var element = document.createElement("li");
     element.classList.add("nav-item");
-    let reference = document.createElement("a");
+    var reference = document.createElement("a");
     reference.classList.add("nav-link");
 
     if (destination.reference === current) {
